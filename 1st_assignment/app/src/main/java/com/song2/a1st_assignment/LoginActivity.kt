2afe2bda3d17119c6faa9ffde788dcb8f75319f1
seunghee63 <0603yang@naver.com>
2019-04-09 23:48:01 +0900

    add function

--- 1st_assignment/app/src/main/java/com/song2/a1st_assignment/LoginActivity.kt
+++ 1st_assignment/app/src/main/java/com/song2/a1st_assignment/LoginActivity.kt
@@ -2,11 +2,45 @@ package com.song2.a1st_assignment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        et_login_id.setOnFocusChangeListener { v, hasfocus ->
+            if (hasfocus == true)
+                v.setBackgroundResource(R.drawable.yellow_border)
+            else
+                v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        et_login_pw.setOnFocusChangeListener { v, hasfocus ->
+            if (hasfocus == true)
+                v.setBackgroundResource(R.drawable.yellow_border)
+            else
+                v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        btn_login_submit.setOnClickListener {
+            if(et_login_id.text.toString() == "")  {
+                et_login_id.setBackgroundResource(R.drawable.yellow_border)
+                et_login_pw.setBackgroundResource(R.drawable.gray_border)
+
+            }else if(et_login_pw.text.toString() == "") {
+                et_login_pw.setBackgroundResource(R.drawable.yellow_border)
+                et_login_id.setBackgroundResource(R.drawable.gray_border)
+            }
+            else
+                startActivity<MainActivity>()
+        }
+
+        tv_login_signup.setOnClickListener {
+            startActivity<SignupActivity>()
+        }
     }
 }
--- 1st_assignment/app/src/main/java/com/song2/a1st_assignment/SignupActivity.kt
+++ 1st_assignment/app/src/main/java/com/song2/a1st_assignment/SignupActivity.kt
@@ -2,11 +2,51 @@ package com.song2.a1st_assignment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        et_signup_id.setOnFocusChangeListener { v, hasfocus ->
+            if (hasfocus == true)
+                v.setBackgroundResource(R.drawable.yellow_border)
+            else
+                v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        et_signup_pw.setOnFocusChangeListener { v, hasfocus ->
+            if (hasfocus == true)
+                v.setBackgroundResource(R.drawable.yellow_border)
+            else
+                v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        et_signup_name.setOnFocusChangeListener { v, hasfocus ->
+            if (hasfocus == true)
+                v.setBackgroundResource(R.drawable.yellow_border)
+            else
+                v.setBackgroundResource(R.drawable.gray_border)
+        }
+
+        btn_signup_submit.setOnClickListener {
+            if(et_signup_id.text.toString() == "")  {
+                et_signup_id.setBackgroundResource(R.drawable.yellow_border)
+                et_signup_pw.setBackgroundResource(R.drawable.gray_border)
+                et_signup_name.setBackgroundResource(R.drawable.gray_border)
+            }else if(et_signup_pw.text.toString() == "") {
+                et_signup_pw.setBackgroundResource(R.drawable.yellow_border)
+                et_signup_id.setBackgroundResource(R.drawable.gray_border)
+                et_signup_name.setBackgroundResource(R.drawable.gray_border)
+            }else if(et_signup_name.text.toString() == "") {
+                et_signup_name.setBackgroundResource(R.drawable.yellow_border)
+                et_signup_id.setBackgroundResource(R.drawable.gray_border)
+                et_signup_pw.setBackgroundResource(R.drawable.gray_border)
+            }
+            else
+                finish()
+        }
     }
 }
